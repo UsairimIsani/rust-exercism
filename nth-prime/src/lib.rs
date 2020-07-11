eb5c40ae33f75517f4caf0d52b7b8b6d3a23d9d8
Usairim Isani <usairim.isani@outlook.com>
2020-07-11 18:25:16 +0500

    Cant Figure Out how it works.

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,50 @@
+// fn is_prime(n: u32) -> bool {
+//     for i in 3..n {
+//         if n % i == 0 {
+//             return false;
+//         }
+//     }
+//     true
+// }
+// pub fn nth(n: u32) -> u32 {
+//     if n == 0 {
+//         2
+//     } else {
+//         let mut count = 0;
+//         let mut res: u32 = 2;
+//         while count < n {
+//             res += 1;
+//             if is_prime(res) {
+//                 count += 1;
+//             }
+//         }
+//         return res;
+//     }
+//     // unimplemented!("What is the 0-indexed {}th prime number?", n)
+// }
+fn is_prime(n: u32) -> bool {
+    let mut i = 3;
+    while (i * i) < (n + 1) {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    if n == 0 {
+        2
+    } else {
+        let mut count = 0;
+        let mut candidate = 1;
+        while count < n {
+            candidate += 2;
+            if is_prime(candidate) {
+                count += 1;
+            }
+        }
+        candidate
+    }
 }
