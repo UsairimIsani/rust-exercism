e94085e84b97033a3c889e78527056433ef98707
Usairim Isani <usairim.isani@outlook.com>
2020-07-17 17:17:39 +0500

    Non Funtional Implementation.

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,26 @@
+use std::collections::HashSet;
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    // Non Functional Implementation
+    let limit = limit as i32;
+    let mut set: HashSet<i32> = HashSet::new();
+    for i in factors {
+        if i == &0 {
+            break;
+        }
+        for j in 1.. {
+            let res = (i * j) as i32;
+            if res < limit {
+                set.insert(res);
+            } else {
+                break;
+            }
+        }
+    }
+    let sum: i32 = set.into_iter().sum();
+    sum as u32
+}
+#[cfg(test)]
+#[test]
+fn some_thing() {
+    assert_eq!(23, sum_of_multiples(10, &[3, 5]))
 }
